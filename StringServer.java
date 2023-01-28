3126bc7ce38b1e1de3223ce76e1be97c93a6d642
rug005 <rug005@ucsd.edu>
2023-01-28 11:27:52 -0800

    Created StringServer
    StringServer is fixed to support the path and behavior explained in Week 3's instructions.

---
+++ StringServer.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+    String stores = "";
+
+    public String handleRequest(URI url) {
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+
+                if (parameters[0].equals("s")) {
+                    stores += parameters[1] + "\n";
+                    return stores;
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
